5de0544bd16b017de65e99a4866d49d40e0c3ba3
Viraj Chandra <95118745+virajchandra51@users.noreply.github.com>
2022-08-23 22:15:33 +0530

    Time: 16 ms (8.42%), Space: 6.8 MB (5.57%) - LeetHub

---
+++ 925-long-pressed-name/925-long-pressed-name.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    bool isLongPressedName(string name, string typed) {
+        string s="";
+        vector<pair<char,int>> v,d;
+        
+        int j=0; int c=0;
+        for(int i=0;i<name.size();i++)
+        {
+            if(name[i]==name[j])
+            {
+                c++;
+            }
+            else
+            {
+                j=i;
+                v.push_back({name[i-1],c});
+                c=1;
+            }
+        }
+        v.push_back({name[name.size()-1],c});
+        for(auto it:v)
+            cout<<it.first<<" "<<it.second<<endl;
+        j=0,c=0;
+        for(int i=0;i<typed.size();i++)
+        {
+            if(typed[i]==typed[j])
+            {
+                c++;
+            }
+            else
+            {
+                j=i;
+                d.push_back({typed[i-1],c});
+                c=1;
+            }
+        }
+        d.push_back({typed[typed.size()-1],c});
+        for(auto it:d)
+            cout<<it.first<<" "<<it.second<<endl;
+        if(v.size()!=d.size())
+            return false;
+        else
+        {
+            for(int i=0;i<v.size();i++)
+            {
+                if(d[i].second<v[i].second || d[i].first!=v[i].first)
+                    return false;
+            }
+            return true;
+        }
+        return true;
+    }
+};
