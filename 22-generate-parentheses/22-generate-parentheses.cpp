005a127f3684126c5f9192bea84c8b647d63a6a1
Viraj Chandra <95118745+virajchandra51@users.noreply.github.com>
2022-09-03 20:22:22 +0530

    Time: 3 ms (87.49%), Space: 14 MB (31.69%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
-    vector<string> ans;
+    vector<string> v;
     void G(int &n, int open, int close, string s)
     {
         if(open==n && close==n)
         {
-            ans.push_back(s);
+            v.push_back(s);
             return;
         }
         if(open<n)
-        {
-            s+="(";
-            G(n,open+1,close,s);
-            s.pop_back();
-        }
+            G(n,open+1,close,s+"(");
         if(close<open)
-        {
-            s+=")";
-            G(n,open,close+1,s);
-            s.pop_back();
-        }
+            G(n,open,close+1,s+")");
     }
     vector<string> generateParenthesis(int n) {
         G(n,0,0,"");
-        return ans;
+        return v;
     }
 };
