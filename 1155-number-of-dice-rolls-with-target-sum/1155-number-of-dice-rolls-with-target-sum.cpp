9f6035c214958795aa255fc565e57979f3eb11e5
Viraj Chandra <95118745+virajchandra51@users.noreply.github.com>
2022-10-02 23:13:31 +0530

    Time: 270 ms (5.19%), Space: 8.5 MB (45.97%) - LeetHub

---
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int M = 1000000007;
+    // dp [i][j] = number of ways to make sum j using first i dices out of n
+    // dp[n][targetSum] = ans
+    // dp [i][j] += dp[i-1][targetSum]
+    vector<vector<int>> dp;
+    int numRollsToTarget(int n, int k, int target) {
+        dp.resize(n+1,vector<int>(target+1));
+        for(int i=1;i<=(min(k,target));i++)
+            dp[1][i]=1;
+        
+        
+        for(int i=2;i<=n;i++)
+        {
+            for(int j=1;j<target+1;j++)
+            {
+                for(int z=max(0,j-k);z<j;z++)
+                    dp[i][j] = (dp[i][j]%M +dp[i-1][z]%M)%M;
+            }
+        }
+        
+        return dp[n][target];
+    }
+};
