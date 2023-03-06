501fda9fa13ca8eb3e2004d1ac2450352fe08dba
Viraj Chandra <95118745+virajchandra51@users.noreply.github.com>
2023-03-06 13:02:38 +0530

    Time: 196 ms (66.67%), Space: 9 MB (73.28%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,34 +1,20 @@
 class Solution {
 public:
-    vector<int> ans;
-    void pre()
-    {
-        for(int i=1;i<=100;i++)
-            ans.push_back(i*i);
-        return;
+    int solve(int curr, int n, vector<int>&dp){
+        if(n==0)
+            return 0;
+        if(curr*curr>n)
+            return 1e5;
+        if(dp[n]!=-1)
+            return dp[n];
+        int take=1e5; 
+        int notake=solve(curr+1, n, dp);
+        if(curr*curr<=n)
+            take=1+solve(curr, n-(curr*curr), dp);
+        return dp[n]=min(take, notake);
     }
     int numSquares(int n) {
-        pre();
-        vector<int> dp(n+1,0);
-        dp[0]=0;
-        dp[1]=1;
-        if(n==1)
-            return 1;
-        for(int i=2;i<=n;i++)
-        {
-            int res=INT_MAX;
-            for(int j=0;j<ans.size();j++)
-            {
-                if((i-ans[j])>=0)
-                {
-                    res = min(res,dp[i-ans[j]]+1);
-                }
-            }
-            dp[i]=res;
-        }
-        for(auto it:dp)
-            cout<<it<<" ";
-        cout<<endl;
-        return dp[n];
+        vector<int>dp(n+1, -1);
+        return solve(1, n, dp);
     }
 };
