dad8188e951f2d2386b7b2601ae284d74021b974
Viraj Chandra <95118745+virajchandra51@users.noreply.github.com>
2023-03-09 11:46:52 +0530

    Time: 408 ms (94.76%), Space: 89.8 MB (86.98%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -2,13 +2,12 @@ class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& i) {
         sort(i.begin(),i.end(),[](vector<int> &a, vector<int> &b) {return a[1]<b[1];});
-        vector<int> prev= i[0];
-        int ans = -1;
-		for(vector<int> it: i) {
-			if(prev[1]>it[0]) {
-				ans++;                //we dont update previous, because i[1] will be grater then prev[1]
-			}else prev=it;           // we want the end point to be minimum
-		}
-		return ans; 
+        int n=i.size(), ans=-1, prev=i[0][1];
+        for(int j=0;j<n;j++){
+            if(i[j][0]<prev){
+                ans++;
+            }
+            else prev=i[j][1];
+        }return ans;
     }
 };
