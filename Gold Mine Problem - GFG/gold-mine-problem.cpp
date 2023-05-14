e53a48e26dd4328077fddc40b020c6f4327ab87b
Viraj Chandra <95118745+virajchandra51@users.noreply.github.com>
2023-05-15 00:31:56 +0530

    Added solution - LeetHub

---
+++ Gold Mine Problem - GFG/gold-mine-problem.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int maxGold(int n, int m, vector<vector<int>> M)
+    {
+        for(int i=1;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                int a = M[j][i-1];
+                if(j-1>=0)
+                a=max(a,M[j-1][i-1]);
+                if(j+1<n)
+                a=max(a,M[j+1][i-1]);
+                M[j][i]+=a;
+            }
+        }
+        int a=INT_MIN;
+        for(int i =0;i<n;i++)
+        a=max(a,M[i][m-1]);
+        
+        return a;
+        // code here
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n, m;
+        cin>>n>>m;
+        vector<vector<int>> M(n, vector<int>(m, 0));
+        for(int i = 0;i < n;i++){
+            for(int j = 0;j < m;j++)
+                cin>>M[i][j];
+        }
+        
+        Solution ob;
+        cout<<ob.maxGold(n, m, M)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
