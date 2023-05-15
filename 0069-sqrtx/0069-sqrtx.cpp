951a18323f98e302222fcedfd1209cc7c7d80190
Viraj Chandra <95118745+virajchandra51@users.noreply.github.com>
2023-05-16 02:02:25 +0530

    Time: 0 ms (100.00%), Space: 6 MB (7.76%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -2,16 +2,22 @@ class Solution {
 public:
     int mySqrt(int x) {
         long l=1,r=x;
+        if(x==1) return 1;
+        if(x<2) return 0;
+        int ans;
         while(l<=r)
         {
-            long m = (l+r)/2;
-            if((m*m)==(x))
-                return (int)m;
-            else if((m*m)<(x))
+            int m = (l+r)>>1;
+            if((m)<=(x/m))
+            {
+                ans=m;
                 l=m+1;
+            }
             else
+            {
                 r=m-1;
+            }
         }
-        return r;
+        return ans;
     }
 };
