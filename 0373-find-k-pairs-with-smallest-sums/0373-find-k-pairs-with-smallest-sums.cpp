5cb134fa0b26c9469c171c303e5f4be2d87e72c1
Viraj Chandra <95118745+virajchandra51@users.noreply.github.com>
2023-06-27 20:55:43 +0530

    Time: 303 ms (70.96%), Space: 114.2 MB (53.44%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -1,36 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-        int m = nums1.size();
-        int n = nums2.size();
-
-        vector<vector<int>> ans;
-        set<pair<int, int>> visited;
-
-        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>,
-                       greater<pair<int, pair<int, int>>>> minHeap;
-        minHeap.push({nums1[0] + nums2[0], {0, 0}});
-        visited.insert({0, 0});
-
-        while (k-- && !minHeap.empty()) {
-            auto top = minHeap.top();
-            minHeap.pop();
-            int i = top.second.first;
-            int j = top.second.second;
-
-            ans.push_back({nums1[i], nums2[j]});
-
-            if (i + 1 < m && visited.find({i + 1, j}) == visited.end()) {
-                minHeap.push({nums1[i + 1] + nums2[j], {i + 1, j}});
-                visited.insert({i + 1, j});
-            }
-
-            if (j + 1 < n && visited.find({i, j + 1}) == visited.end()) {
-                minHeap.push({nums1[i] + nums2[j + 1], {i, j + 1}});
-                visited.insert({i, j + 1});
-            }
+        set<pair<int,int>> s;
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> p;
+        long n = nums1.size();
+        long m = nums2.size();
+        int t = n*m;
+        s.insert({0,0});
+        p.push({nums1[0]+nums2[0],{0,0}});
+        vector<vector<int>> a;
+        while(k-- && !p.empty())
+        {
+            auto it = p.top();
+            p.pop();
+            int su = it.first;
+            int i = it.second.first;
+            int j = it.second.second;
+            a.push_back({nums1[i],nums2[j]});
+            if(i+1<n && s.find({i+1,j})==s.end())
+            {p.push({nums1[i+1]+nums2[j],{i+1,j}});s.insert({i+1,j});}
+            if(j+1<m && s.find({i,j+1})==s.end())
+            {p.push({nums1[i]+nums2[j+1],{i,j+1}});s.insert({i,j+1});}
         }
-
-        return ans;
+        return a;
     }
 };
