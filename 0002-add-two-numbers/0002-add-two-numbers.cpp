9b65e266186c944b9e047851b1f04bcb0d9f1aea
Viraj Chandra <95118745+virajchandra51@users.noreply.github.com>
2023-07-04 17:01:18 +0530

    Time: 31 ms (88.02%), Space: 71.4 MB (28.00%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -11,28 +11,45 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* dummy=new ListNode(0);
-        ListNode* curr=dummy;
-        int x,y,carry=0;
-        while(l1 || l2 || carry)
+        ListNode* n=new ListNode(-1);
+        ListNode* nh=n;
+        ListNode* carry=new ListNode(0);
+        while(l1&&l2)
         {
-            x=l1 ? l1->val:0;
-            y=l2 ? l2->val:0;
-            int sum = carry+x+y;
-            curr->next = new ListNode(0);
-            curr->val = sum%10;
-            carry = sum/10;
-            curr = curr->next;
-            l1=l1?l1->next:NULL;
-            l2=l2?l2->next:NULL;   
+           int c=l1->val+l2->val+carry->val;
+           int x=c%10;
+           ListNode* s=new ListNode(x);
+           c=c/10;
+           n->next=s;
+           carry->val= c;
+           n=n->next;
+           l1=l1->next;
+           l2=l2->next;
         }
-        curr=dummy;
-        while(curr->next->next!=NULL)
-            curr=curr->next;
-        if(carry)
-            curr->next->val=1;
-        else
-            curr->next=NULL;
-        return dummy;
+        while(l1)
+        { 
+            int c=l1->val+carry->val;
+             ListNode* s=new ListNode(c%10);
+              c=c/10;
+               n->next=s;
+            n=n->next;
+                carry->val= c;
+                 l1=l1->next;
+                 
+        }
+       
+        while(l2)
+{
+    int c=l2->val+carry->val;
+             ListNode* s=new ListNode(c%10);
+              c=c/10;
+               n->next=s;
+            n=n->next;
+                carry->val= c;
+                 l2=l2->next;
+    }
+    if(carry->val==1)
+    n->next=carry;
+    return nh->next;
     }
 };
