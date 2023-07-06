04bdc9403b415e2ca2d5a7242bc2493cf75f16a0
Viraj Chandra <95118745+virajchandra51@users.noreply.github.com>
2023-07-06 10:05:28 +0530

    Time: 36 ms (90.01%), Space: 28.2 MB (71.68%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,50 +1,19 @@
 class Solution {
 public:
-    long id(vector<int>& a, int st, int n, int t)
-    {
-        int l = st;
-        int r = n-1;
-        long ans=-1;
-        while(l<=r)
-        {
-            int m = (l+r)>>1;
-            if(a[m]>=t)
-            {
-                ans=m;
-                r=m-1;
-            }
-            else
-            {
-                l=m+1;
-            }
-        }
-        return ans;
-    }
     int minSubArrayLen(int target, vector<int>& nums) {
-        long ans = INT_MAX;
-        int n = nums.size();
-        vector<int> a(n);
-        a[0]=nums[0];
-        for(int i=1;i<n;i++) a[i]=a[i-1]+nums[i];
-        for(int i=0;i<n;i++)
-        {
-            if(i==0)
-            {
-                auto it = id(a,i,n,target);
-                if(it==-1)
-                    continue;
-                else
-                    ans=min(ans,it-i+1);
-            }
-            else
-            {
-                auto it = id(a,i,n,target+a[i-1]);
-                if(it==-1)
-                    continue;
-                else
-                    ans=min(ans,it-i+1);
+        int left = 0, right = 0, sumOfCurrentWindow = 0;
+        int res = INT_MAX;
+
+        for(right = 0; right < nums.size(); right++) {
+            sumOfCurrentWindow += nums[right];
+
+            while (sumOfCurrentWindow >= target) {
+                res = min(res, right - left + 1);
+                sumOfCurrentWindow -= nums[left];
+                left++;
             }
         }
-        return ans==INT_MAX?0:ans;
+
+        return res == INT_MAX ? 0 : res;
     }
 };
