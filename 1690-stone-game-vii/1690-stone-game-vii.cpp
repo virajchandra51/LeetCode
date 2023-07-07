efb89012b6236698ea797daf32c7f13b0cf18deb
Viraj Chandra <95118745+virajchandra51@users.noreply.github.com>
2023-07-07 13:58:06 +0530

    Time: 451 ms (33.75%), Space: 84.3 MB (48.29%) - LeetHub

--- 1690-stone-game-vii/1690-stone-game-vii.cpp
+++ 1690-stone-game-vii/1690-stone-game-vii.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    vector<vector<vector<int>>> dp;
+    vector<vector<int>> dp;
     int helper(vector<int>& stones, int i, int j, int turn, int sum)
     {
         if(i>=j) return 0;
-        if(dp[turn][i][j]!=-1) return dp[turn][i][j];
+        if(dp[i][j]!=-1) return dp[i][j];
         int l = sum-stones[i];
         int r = sum-stones[j];
         if(turn)
-        return dp[turn][i][j] = max(l+helper(stones,i+1,j,0,l),r+helper(stones,i,j-1,0,r));
-        return dp[turn][i][j] = min(-l+helper(stones,i+1,j,1,l),-r+helper(stones,i,j-1,1,r));
+        return dp[i][j] = max(l+helper(stones,i+1,j,0,l),r+helper(stones,i,j-1,0,r));
+        return dp[i][j] = min(-l+helper(stones,i+1,j,1,l),-r+helper(stones,i,j-1,1,r));
     }
     int stoneGameVII(vector<int>& stones) {
         int n = stones.size();
-        dp.resize(2,vector<vector<int>>(n,vector<int>(n,-1)));
+        dp.resize(n,vector<int>(n,-1));
         int sum = accumulate(stones.begin(),stones.end(),0);
         return abs(helper(stones,0,n-1,1,sum));
     }
