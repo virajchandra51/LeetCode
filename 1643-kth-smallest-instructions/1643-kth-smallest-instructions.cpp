1a1044232bd25cb8ceaa5f1410e2e285dbc3a8f4
Viraj Chandra <95118745+virajchandra51@users.noreply.github.com>
2023-07-19 23:44:19 +0530

    Time: 3 ms (84.53%), Space: 15.7 MB (29.28%) - LeetHub

---
+++ 1643-kth-smallest-instructions/1643-kth-smallest-instructions.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    string kthSmallestPath(vector<int>& d, int k) {
+        vector<long double> fac(31,1);
+        fac[0]=1;
+        fac[1]=1;
+        for(int i=2;i<31;i++)
+            fac[i]=(i*fac[i-1]);
+        long long currH = d[1];
+        long long currV = d[0];
+        long long total = currH+currV;
+        string a = "";
+        while(total>0)
+        {
+            if(currH>0)
+            {
+                double ifH = fac[total-1]/fac[currH-1]/fac[currV];
+                if(ifH>=k)
+                {
+                    a+="H";
+                    currH--;
+                    total--;
+                }
+                else
+                {
+                    k-=ifH;
+                    a+="V";
+                    currV--;
+                    total--;
+                }
+            }
+            else
+            {
+                for(int i=0;i<currV;i++)
+                    a+="V";
+                break;
+            }
+        }
+        return a;
+    }
+};
