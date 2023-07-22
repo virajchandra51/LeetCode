4b214cf514be1e5c597a5e5a7f3c3913e12fb963
Viraj Chandra <95118745+virajchandra51@users.noreply.github.com>
2023-07-22 20:58:31 +0530

    Time: 14 ms (79.23%), Space: 8.7 MB (67.87%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.cpp
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.cpp
@@ -6,22 +6,21 @@ public:
     }
     double solve(int i,int j,int k,int n,vector<vector<vector<double>>>&dp){
         if(!isInBoard(i, j, n))return 0;
-        if(k==0)return isInBoard(i, j, n);
+        if(k==0) return 1;
         if(dp[i][j][k]!=-1)return dp[i][j][k];
         double ans=0;
-        ans+=solve(i-2, j+1, k-1, n, dp);
-        ans+=solve(i-1, j+2, k-1, n, dp);
-        ans+=solve(i+1, j+2, k-1, n, dp);
-        ans+=solve(i+2, j+1, k-1, n, dp);
-        ans+=solve(i+2, j-1, k-1, n, dp);
-        ans+=solve(i+1, j-2, k-1, n, dp);
-        ans+=solve(i-1, j-2, k-1, n, dp);
-        ans+=solve(i-2, j-1, k-1, n, dp);
+        ans+=(solve(i-2, j+1, k-1, n, dp)/8.0);
+        ans+=(solve(i-1, j+2, k-1, n, dp)/8.0);
+        ans+=(solve(i+1, j+2, k-1, n, dp)/8.0);
+        ans+=(solve(i+2, j+1, k-1, n, dp)/8.0);
+        ans+=(solve(i+2, j-1, k-1, n, dp)/8.0);
+        ans+=(solve(i+1, j-2, k-1, n, dp)/8.0);
+        ans+=(solve(i-1, j-2, k-1, n, dp)/8.0);
+        ans+=(solve(i-2, j-1, k-1, n, dp)/8.0);
         return dp[i][j][k]=ans;
     }
     double knightProbability(int n, int k, int row, int column) {
         vector<vector<vector<double>>>dp(n, vector<vector<double>>(n, vector<double>(k+1, -1)));
-        double ans=solve(row, column, k, n, dp);
-        return ans/pow(8, k);
+        return solve(row, column, k, n, dp);
     }
 };
