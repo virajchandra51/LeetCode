3b2ba25b0f4f2f9d3655df7a0cf18135a5e9d214
Viraj Chandra <95118745+virajchandra51@users.noreply.github.com>
2023-07-23 20:01:37 +0530

    Time: 7 ms (99.43%), Space: 16.7 MB (42.47%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -2,21 +2,18 @@ class Solution {
 public:
     int jump(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n,INT_MAX);
-        dp[n-1]=0;
-        for(int i=n-2;i>=0;i--)
+        int end = 0;
+        int far = 0;
+        int steps = 0;
+        for(int i=0;i<n-1;i++)
         {
-            int mi = INT_MAX;
-            for(int j=i+1;j<=(min(n-1,i+nums[i]));j++)
+            far=max(far,nums[i]+i);
+            if(i==end)
             {
-                mi=min(mi,dp[j]);
+                end=far;
+                steps++;
             }
-            if(mi!=INT_MAX)
-            dp[i] = mi+1;
         }
-        for(auto it:dp)
-            cout<<it<<" ";
-        cout<<endl;
-        return dp[0];
+        return steps;
     }
 };
