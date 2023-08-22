e9198f69d65e04b6d3d0eff945683167bfba488c
Viraj Chandra <95118745+virajchandra51@users.noreply.github.com>
2023-08-22 10:12:37 +0530

    Time: 0 ms (100.00%), Space: 6 MB (37.93%) - LeetHub

--- 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
@@ -3,31 +3,20 @@ public:
     string s = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     string convertToTitle(int columnNumber) {
         string ans="";
-         int remainder=0;
-         
-         while(columnNumber>0){
-            
-           remainder=columnNumber%26;
-           if(remainder==0){
-               ans+=s[25];
-               columnNumber-=26;
-           } 
-           else{
-               ans+=s[remainder-1];
-           } 
-           
-           columnNumber-=remainder;
-             
-           columnNumber=columnNumber/26; 
-           
-          
-
-             
-         }
-        
-         
-         reverse(ans.begin(),ans.end());
-        
+        int remainder=0;
+        while(columnNumber>0){
+            remainder=columnNumber%26;
+            if(remainder==0){
+                ans+=s[25];
+                columnNumber-=26;
+            } 
+            else{
+            ans+=s[remainder-1];
+            } 
+            columnNumber-=remainder;
+            columnNumber=columnNumber/26; 
+        }
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
