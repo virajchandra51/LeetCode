40e522306e465e60318a27befa34cc74562e8d2b
Viraj Chandra <95118745+virajchandra51@users.noreply.github.com>
2023-08-23 22:08:29 +0530

    Time: 24 ms (49.76%), Space: 19.3 MB (33.65%) - LeetHub

--- 0365-water-and-jug-problem/0365-water-and-jug-problem.cpp
+++ 0365-water-and-jug-problem/0365-water-and-jug-problem.cpp
@@ -1,9 +1,24 @@
 class Solution {
 public:
-    bool canMeasureWater(int j1, int j2, int t) {
-        if(j1+j2<t)
-            return false;
-        if(t%__gcd(j1,j2)==0) return true;
+    bool canMeasureWater(int x, int y, int targetCapacity) {
+        int operations[4]= {x,-x,y,-y};
+        queue<int>q;
+        q.push(0);
+        vector<int> visited(x+y+1,0);
+        visited[0]=1;
+        while(!q.empty()){
+           int water=q.front();
+           q.pop();
+           if(water==targetCapacity)
+               return true;
+           for(int i=0;i<4;i++){
+               int newwater=water+operations[i];
+               if(newwater>=0 && newwater<=(x+y) && visited[newwater]==0)                {
+                   visited[newwater]=1;
+                   q.push(newwater);
+               }
+           }
+        }
         return false;
     }
 };
