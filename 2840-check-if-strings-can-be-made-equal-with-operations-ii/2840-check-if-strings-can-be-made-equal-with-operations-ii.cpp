6771d4332c55d694148f405af80729d49c5631ab
Viraj Chandra <95118745+virajchandra51@users.noreply.github.com>
2023-09-03 11:38:33 +0530

    Time: 119 ms (63.64%), Space: 20 MB (27.27%) - LeetHub

---
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool checkStrings(string s1, string s2) {
+        string a = "";
+        string b = "";
+        string c = "";
+        string d = "";
+        int n1 = s1.size();
+        int n2 = s2.size();
+        for(int i=0;i<n1;i++)
+        {
+            if(i&1) a+=s1[i]; else b+=s1[i];
+        }
+        for(int i=0;i<n2;i++)
+        {
+            if(i&1) c+=s2[i]; else d+=s2[i];
+        }
+        sort(a.begin(),a.end());
+        sort(b.begin(),b.end());
+        sort(c.begin(),c.end());
+        sort(d.begin(),d.end());
+        return a==c && b==d;
+    }
+};
