a2ff68c8c7aecb64a2ca9f1d95a2d865cce9e914
Viraj Chandra <95118745+virajchandra51@users.noreply.github.com>
2024-01-27 19:18:48 +0530

    Time: 38 ms (27.84%), Space: 14.4 MB (13.43%) - LeetHub

--- 1541-minimum-insertions-to-balance-a-parentheses-string/1541-minimum-insertions-to-balance-a-parentheses-string.cpp
+++ 1541-minimum-insertions-to-balance-a-parentheses-string/1541-minimum-insertions-to-balance-a-parentheses-string.cpp
@@ -8,32 +8,40 @@ public:
             if(s[i]=='(') st.push(s[i]); // If I have a '(' store it
             else
             {
+                // if I find two consecutive ))
                 if(i+1<s.size() && s[i+1]==')')
                 {
+                    // if for those )) I have a ( in stack no need to increase count
                     if(st.size()>0)
                     {
                         st.pop();
                     }
+                    // if I dont have ( in stack means I need to insert a ( in the string for the two )) I have at s[i] & s[i+1]
                     else
                     {
                         count++;
                     }
+                    // since I know s[i] & s[i+1] have ) kept I can jump two indexes forward to i+2
                     i++;
                 }
+                // if s[i+1] is not equal to )
                 else 
                 {
+                    count++; // I would have to insert a ) to pair up with the s[i] character so that I get two consecutive ))
+                    // if now for the two consecutive )) I have a ( in stack i pop it out
                     if(st.size()>0)
                     {
                         st.pop();
                     }
+                    // If I dont have a (  in stack that means again I need to insert (  in string 
                     else
                     {
                         count++;
                     }
-                    count++;
                 }
             }
         }
+        // if coming out of loop there are still some ( left in stack which did not get their pairs of )) , then I add them to the answer too
         count+=(2*st.size());
         return count;
     }
