c790e93d1fb32079b29f012aa54ecc9e81f4d663
Viraj Chandra <95118745+virajchandra51@users.noreply.github.com>
2024-01-31 22:35:30 +0530

    Time: 91 ms (91.98%), Space: 86.7 MB (42.78%) - LeetHub

--- 2321-maximum-score-of-spliced-array/2321-maximum-score-of-spliced-array.cpp
+++ 2321-maximum-score-of-spliced-array/2321-maximum-score-of-spliced-array.cpp
@@ -2,17 +2,21 @@ class Solution {
 public:
     int maximumsSplicedArray(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size();
+        
         vector<int> diff(n);
-        // 30 0 -40 85 -20 0
         for(int i=0;i<n;i++)
             diff[i] = nums2[i]-nums1[i];
+        
         int sum1 = 0;
         int sum2 = 0;
         for(auto it:nums1) sum1+=it;
         for(auto it:nums2) sum2+=it;
+        
         int answer = max(sum1,sum2);
+        
         int sumKadane = 0;
         int c = 0;
+        // run kadane when maximising sum of nums1
         for(int i=0;i<n;i++)
         {
             sumKadane+=diff[i];
@@ -23,9 +27,12 @@ public:
             }
             else sumKadane = 0;
         }
+        
+        for(auto &it:diff) it*=-1;
+        
+        // run kadane when maximising sum of nums2
         sumKadane = 0;
         c=0;
-        for(auto &it:diff) it*=-1;
         for(int i=0;i<n;i++)
         {
             sumKadane+=diff[i];
@@ -36,6 +43,7 @@ public:
             }
             else sumKadane = 0;
         }
+        
         return answer;
     }
 };
