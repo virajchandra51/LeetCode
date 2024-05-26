15da15f6d515841b73f576cf76f9d54024eb602c
Viraj Chandra <95118745+virajchandra51@users.noreply.github.com>
2024-05-26 16:22:02 +0530

    Time: 1276 ms (30.77%), Space: 194.6 MB (7.69%) - LeetHub

--- 3164-find-the-number-of-good-pairs-ii/3164-find-the-number-of-good-pairs-ii.cpp
+++ 3164-find-the-number-of-good-pairs-ii/3164-find-the-number-of-good-pairs-ii.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
     long long numberOfPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-        vector<int> freq(1000001, 0);
-        map<int, int> mp;
-        for (int x : nums2) 
-            mp[x]++;
-        for (auto x : mp)
-            for (int i = x.first; i <= 1e6; i += x.first)
-                freq[i] += x.second;
-        long long res = 0;
-        for (int x : nums1)
-            if (x % k == 0)
-                res += freq[x / k];
-        return res;
+        unordered_map<int,int> hashTable;
+        for(auto number : nums1){
+            for(int i = 1; i * i <= number; i++){
+                if(i * i == number){
+                    hashTable[i]++;
+                }else if(number % i == 0){
+                    hashTable[i]++;
+                    hashTable[number / i]++;
+                }
+            }
+            
+        }
+        long long int answer = 0;
+        for(auto number : nums2){
+            number *= k;
+            if(hashTable[number] > 0){
+                answer += hashTable[number];
+            }
+        }
+        return answer;
     }
 };
